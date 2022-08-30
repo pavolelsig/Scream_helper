649cf8713da9911c169e3d97ff0c25f0af4b5999
Nicolas <52626965+Iceburgino@users.noreply.github.com>
2022-08-30 13:44:17 +0300

    Improvements
    Sometimes multiple vms use different networks, but the script listens to only virbr0. I propose changes that make helper run a script that finds all network bridge devices, and saves them to /etc/scream/devices. Then, when scream-audio is run on startup, it reads this list and launches scream for each bridge device.
    
    The script also detects whether the given bridge device is already used by scream and does not run new instances for this device.
    
    Because of this, if you create any new networks, you can execute scream-reconfigure and then scream-audio from the terminal to run audio for new networks.
    
    if you want to choose which bridge devices to use by yourself, don't run scream-reconfigure, but instead edit /etc/scream/devices to manually list all the needed devices.
    
    I also renamed some files to make them more pleasant to run from the terminal (I think .sh extension is unnecessary - running scream-audio from shell without .sh, just like any command, such as sed, ls or grep, feels natural).
    
    Apart from that, Arch support is enabled just by adding another condition to Manjaro pacman installation.

--- audio.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/scream -i virbr0
--- helper.sh
+++ helper.sh
@@ -36,7 +36,12 @@ cp audio.sh.desktop /home/$THE_USER/.config/autostart/
 chown $THE_USER /home/$THE_USER/.config/autostart/audio.sh.desktop
 
 #This will be used to autostart scream and can be edited if different parameters are used
-cp audio.sh /usr/bin/scream_audio.sh
+cp scream-audio /usr/bin/scream-audio
+
+#This will be used to retrieve all bridge devices on this computer, 
+# save them to /etc/scream/devices, 
+# and use found devices for scream later
+cp scream-reconfigure /usr/bin/scream-reconfigure
 
 #Installing required packages
 
@@ -45,15 +50,15 @@ FEDORA=`cat /etc/*release |  head -n 1 | cut -d ' ' -f 1`
 
 if [ "$DISTRO" == "Ubuntu" ] || [ "$DISTRO" == "Pop" ] || [ "$DISTRO" == "LinuxMint" ]
 	then
-apt install libpulse-dev make cmake libasound2-dev -y
-elif [ "$DISTRO" == "ManjaroLinux" ]
+apt install libpulse-dev make cmake screen libasound2-dev  -y
+elif [ "$DISTRO" == "ManjaroLinux" ] || [ "$DISTRO" == "Arch" ]
 	then
-pacman -Syu cmake make base-devel
+pacman -Syu cmake make base-devel screen
 elif [ "$FEDORA" == "Fedora" ]
 	then
-dnf install cmake make gcc pulseaudio-libs-devel alsa-lib-devel libpcap-devel
+dnf install cmake make gcc screen pulseaudio-libs-devel alsa-lib-devel libpcap-devel
 	else
-echo "This script does not support your current distribution. Only Ubuntu, PopOS, Manjaro, and Mint are supported!"
+echo "This script does not support your current distribution. Only Ubuntu, PopOS, Manjaro, Arch and Mint are supported!"
 echo "You can still install Looking Glass manually!"
 	exit
 fi
@@ -72,11 +77,15 @@ make
 #Moving files to their expected locations
 mv scream /usr/bin/scream
 
+#Adding permissions to run copied files
 chmod +x /usr/bin/scream
+chmod +x /usr/bin/scream-audio
+chmod +x /usr/bin/scream-reconfigure
 
-
-chmod +x /usr/bin/scream_audio.sh
-
+#Detecting and saving bridge devices for scream to use.
+#If you want to specify them manually,
+# edit /etc/scream/devices to change list of devices for scream to use
+scream-reconfigure
 
 #In Fedora Scream is normally blocked by the firewall - opening up a port
 if [ "$FEDORA" == "Fedora" ]
